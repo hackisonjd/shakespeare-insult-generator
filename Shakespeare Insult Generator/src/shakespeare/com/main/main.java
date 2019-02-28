d672262843b669268f9f4b2349dbd086508910e2
hackisonjd <48108009+hackisonjd@users.noreply.github.com>
2019-02-28 18:23:09 -0500

    Add files via upload

---
+++ Shakespeare Insult Generator/src/shakespeare/com/main/main.java
@@ -0,0 +1,18 @@
+package shakespeare.com.main;
+
+import java.util.Random;
+
+public class main {
+    public static void main(String[] args) {
+        final String[] firstColumn = new String[] {"artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerheaded", "lumpish", "mammering", "mangled", "mewling", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"};
+        final String[] secondColumn = new String[] {"base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-patched", "crook-pated", "dismal-dreaming", "doghearted", "dread-bolted", "earth-vexing", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hall-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten" };
+        final String[] thirdColumn = new String[] {"apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpile", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-grill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-wurm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"};
+
+        Random random = new Random();
+        int index1 = random.nextInt(firstColumn.length);
+        int index2 = random.nextInt(secondColumn.length);
+        int index3 = random.nextInt(thirdColumn.length);
+
+        System.out.println("Thou " + firstColumn[index1] + " " + secondColumn[index2] + " " + thirdColumn[index3] + "!");
+    }
+}
